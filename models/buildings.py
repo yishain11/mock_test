80f22e82474a572cbbc006ec8f711b2eaa779419
yishain11 <yishain11@walla.co.il>
2025-11-24 16:23:45 +0200

    added clases for buildings and rooms

--- models/buildings.py
+++ models/buildings.py
@@ -1,6 +1,13 @@
-class Building:
-    pass
+from models.soldiers import Soldier
 
 
 class Room:
-    pass
+    max_occpiers: int
+    current_residances: list[Soldier]
+    current_num_of_residances: int
+
+
+class Building:
+    room_num: int = 8
+    waiting_list: list[Soldier] = []
+    rooms: list[Room]
